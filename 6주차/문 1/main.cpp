6507b81b37d21cca19271d7332f469d9931804a4
김윤수 <112995075+crissPbacon@users.noreply.github.com>
2022-10-15 01:38:36 +0900

    5주차 1번문제 완료
    for문과 배열 올바르게 사용하기

---
+++ 6주차/문 1/main.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#define password 0+0+1+1+1+8	// correct password is my birthday, 001118......
+
+int password_func(void);	
+
+int password_func(void)			// password_func works like this......
+{
+	int ary[6];
+	int i;
+	int res = 0;
+
+	printf("input password......\n");
+
+	for (i = 0; i < 6; i++)		// input numbers......
+	{
+		scanf_s("%d", &ary[i]);
+	}
+
+	for (i = 0; i < 6; i++)		// plus all input numbers and remember......
+	{
+		res += ary[i];
+	}
+
+	for (i = 0; i < 6; i++)		// print input numbers......
+	{
+		printf("%4d", ary[i]);
+	}
+
+	printf("\n");
+
+	if (res == password)
+	{
+		printf("normal termination......\n");
+	}
+	else if (res < password)
+	{
+		printf("not matched, retry......\n\n\n");
+		return password_func();		//  input numbers again......
+	}
+}
+
+
+int main(void)		// this is main......
+{
+	password_func();		// call password_func
+	printf("well done!\n\n");
+
+	return 0;
+}
